17487f94db2198904cebd79b374c27a95b4a0785
Arik Bidny <arikbidny@gmail.com>
2021-03-24 12:01:45 +0200

    initial commit

---
+++ src/main/java/com/arikbidny/basicfordevops/BasicfordevopsApplication.java
@@ -0,0 +1,13 @@
+package com.arikbidny.basicfordevops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicfordevopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicfordevopsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arikbidny/basicfordevops/BasicfordevopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arikbidny.basicfordevops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicfordevopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
